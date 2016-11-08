5d5ad88f47358d9d160886a03cec4fd6b10e465d
iMager85 <jansen12p@gmail.com>
2016-11-08 12:34:21 +0100

    Updated

---
+++ Forraskod/SQlite_Example/SQlite_Example/Adatbazis.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO; //ezt a névteret használom arra ,hogy meghatározzam az adatbázis fájl mappáját (amely itt található /projekt/debug/Adatbazis - ezt akarjuk dinamikusan
+//meghatározni
+//az adatbázis kapcsolathoz szükséges névtér(ek)
+using System.Data.SQLite;
+
+namespace SQlite_Example
+{
+    class Adatbazis
+    {
+        //felfesszük private-ként ,hogy csak belül tudjuk használni de ott minden metóduson belül
+        SQLiteConnection conn;
+
+        #region singleton
+        /// <summary>
+        /// Adatbázis példányosításakor megnyitjuk a kapcsolatot.
+        /// </summary>
+        private Adatbazis() {
+            //string melyel elérjük az adatbázist (nem használunk direkt linket - hanem a program futási környezetből keressük meg az 
+            // adatbázis fájlt - mert ha telepítjük máshova a programot akkor át kéne írni a direkt linket ...)
+            string adatbEleres = Directory.GetCurrentDirectory() + @"\Adatbazis\Adatbazis.db"; //currentdirectory meghatározza a jelenlegi futtatási környezetet - az az azt az elérést
+            //ahol maga a futtatható exe van! ehhez kell hozzáfűzni a mi elérésünk az adatbzishoz
+
+            //ezen az elérése létre hozzuk a 
+            //az eléréshez szükség van a látható stringre - version az a driver verziója az 3 .....
+            conn = new SQLiteConnection(string.Format("Data Source = {0};Version = 3;", adatbEleres));
+            //továbbá lehet itt olyanokat megoldani ,hogy elsőnek megnézi ,hogy létezik-e ez a .db db fájl és ha nem létezik akkor
+            // create table ....... stb-kkel elkészít a táblát a felhasználónak (parktikus első indításkor) - vagy a projekt telepítőjébe
+            // bele kell integrálni majd az adatbázist az adott elérésen -- prjekt\adatbazis\adatbazis.db
+            conn.Open();
+
+        }
+        private static Adatbazis peldany = null;
+        /// <summary>
+        /// Amikor az adatbázist elkészítjük, akkor egyből meg is nyitjuk
+        /// </summary>
+        /// <returns></returns>
+        public static Adatbazis GetPeldany() {
+            if (peldany == null)
+            {
+                peldany = new Adatbazis();
+            }
+            return peldany;
+        }
+
+        /// <summary>
+        /// Paraméterben meg adhatod a lekerdezest (példa ->"select * from tablanev") Fontos ,hogy itt csak selectek lehetnek
+        /// Hiszen ez csak executeReader-t használ - és ez readerrel tér vissza - 
+        /// </summary>
+        /// <param name="lekerdezes"></param>
+        /// <returns></returns>
+        public SQLiteDataReader Lekerdezes(string lekerdezes) {
+            SQLiteCommand comm = new SQLiteCommand(lekerdezes,conn); //itt a végrehajtandó lekérdezést kell átadni illetve ,hogy milyen adatbázison
+            SQLiteDataReader reader = comm.ExecuteReader(); //végre hajta a beolvasást
+
+            return reader; //vissza adod ezt a readert (ami ilyenkor nyitva is lesz!)
+        }
+        #endregion
+
+    }
+}
--- Forraskod/SQlite_Example/SQlite_Example/Form1.cs
+++ Forraskod/SQlite_Example/SQlite_Example/Form1.cs
@@ -8,6 +8,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+//itt is be kell usingolni , mert az SQLiteDataReader ebbe a névtérbe van
+using System.Data.SQLite;
+
 namespace SQlite_Example
 {
     public partial class Form1 : Form
@@ -15,6 +18,38 @@ namespace SQlite_Example
         public Form1()
         {
             InitializeComponent();
+            
+            //ezzzel példányosítod az adatbázist
+            Adatbazis db = Adatbazis.GetPeldany();
+
+            //utána tudod használni
+            //nézzünk egy inner join-os (foreign key) lekérdezést
+            //ezt ki lehet próbálgatni a DB Browser for Sqlite programban (vagy a firefox-os kiegészítőben)
+            //utána meg már csak be kell ide másolni az sql lekérdezés
+            SQLiteDataReader reader = db.Lekerdezes("select fh.nev as 'Felhasználó' ,va.nev as 'Lakhely' from Felhasznalo fh inner join Varos va on fh.Lakhely = va.Iranyitoszam;");
+
+            //majd feldolgozzuk (ezt felhasználó válogatja ,hogy mit akar feldolgozni - az az , hogy hova iratja ki vagy hova nem ..)
+            //most csak logoltatom
+
+            while (reader.Read())
+            {
+                //a reader beolvas egy sort majd azt kell indexelni ,hogy annak hanyadik oszlopában lévő adatra vagyunk kiváncsiak
+                //field count megadja ,hogy hány oszlopunk van -> ez esetben most a lekérdezésnek 2 (az az indexelendő 0,1)
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    //a readernek van getint , getstring .... stb de ezeknek mindenképpen ilyet kell tudni be olvasni az adatbázisból
+                    //viszont ha nem vagyunk biztosak abban amit beolvasunk akkor ott a get value - stringesítjük és utána lehet kezelni ahogy akarjuk
+                    // i = 0 -> első oszlop az a név ez esetben
+                    // i = 1 -> ez pedig a lakhelye ez esetben
+                    //a getname adja vissza az oszlop nevét
+                    MessageBox.Show(string.Format("{0}: {1}",reader.GetName(i),reader.GetValue(i).ToString()));
+                }
+                //de természetesen megoldható ,hogy az egész sort írja ki
+                //ez egy adott sornak MINDEN értékét írja ki egyből
+                //ilyenkor egy stringhez fűzödhozzá vagy lista vagy valami hasonló és azt iratod ki
+
+            }
+
         }
     }
 }
